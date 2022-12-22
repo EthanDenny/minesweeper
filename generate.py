49069e51cf2e49c93b0ad2c7c5526a1d8e9f64df
EthanDenny <ethandenny@protonmail.com>
2022-12-21 21:10:48 -0330

    Updated the way mines are represented
    Uses booleans instead of integers to represent mines

--- generate.py
+++ generate.py
@@ -1,8 +1,8 @@
 import random
 
 """
-Returns a 2D list with 1's marking mines
-and 0's marking safe spaces
+Returns a 2D list with True marking mines
+and False marking safe spaces
 """
 def generate(mines=99, width=30, height=16):
     remaining_squares = width * height
@@ -12,9 +12,9 @@ def generate(mines=99, width=30, height=16):
         for x in range(width):
             if random.randint(0, remaining_squares) < mines:
                 mines -= 1
-                row.append(1)
+                row.append(True)
             else:
-                row.append(0)
+                row.append(False)
             remaining_squares -= 1
         field.append(row)
     return field
